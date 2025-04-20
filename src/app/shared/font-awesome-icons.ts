e226254718c167df605ea465b25c03e4eac3ac97
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-20 16:30:27 +0700

    chore: update font

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,10 +1,13 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { faBook, faCirclePlay, faHome, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faChevronLeft, faChevronRight, faCirclePlay, faHome, faPlus, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
     faCirclePlay,
     faHome,
     faSearch,
     faBook,
-    faPlus
+    faPlus,
+    faChevronLeft,
+    faChevronRight,
+    faUser
 ]
