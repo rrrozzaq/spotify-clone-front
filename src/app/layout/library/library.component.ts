92182709bed601bcb0149932ac3639c767b234b5
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-23 21:40:17 +0700

    fix: add song can't route solved

--- src/app/layout/library/library.component.ts
+++ src/app/layout/library/library.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-library',
-  imports: [FontAwesomeModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss',
 })
