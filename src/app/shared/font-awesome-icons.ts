841cacf06ddb870f3178f90d58d4501f078820ac
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-23 21:39:40 +0700

    ui: add icon

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,13 +1,26 @@
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import { faBook, faChevronLeft, faChevronRight, faCirclePlay, faHome, faPlus, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  faBook,
+  faChevronLeft,
+  faChevronRight,
+  faCircleNotch,
+  faCirclePlay,
+  faCircleXmark,
+  faHome,
+  faPlus,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
-    faCirclePlay,
-    faHome,
-    faSearch,
-    faBook,
-    faPlus,
-    faChevronLeft,
-    faChevronRight,
-    faUser
-]
+  faCirclePlay,
+  faHome,
+  faSearch,
+  faBook,
+  faPlus,
+  faChevronLeft,
+  faChevronRight,
+  faUser,
+  faCircleNotch,
+  faCircleXmark,
+];
