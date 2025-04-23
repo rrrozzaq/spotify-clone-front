e2f2d2286d852cd2e349f6c05550e9187577a228
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-04-23 21:40:35 +0700

    feat: add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { AddSongComponent } from './add-song/add-song.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: "add-song",
+        component: AddSongComponent
+      }
+];
